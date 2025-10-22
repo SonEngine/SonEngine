ec2c91b593162e33d3ecabb62cb172e5bd348235
Soku3D <214961477+SonEngine@users.noreply.github.com>
2025-10-22 21:34:51 +0900

    Create MultiThreadApp.cpp

---
+++ SonEngine/MultiThreadApp.cpp
@@ -0,0 +1,505 @@
+#include "MultiThreadApp.h"
+#include "RootSignature.h"
+#include "PipelineState.h"
+#include "GeometryGenerater.h"
+
+#include "Directxtk12/DDSTextureLoader.h"
+#include "directxtk12/ResourceUploadBatch.h"
+
+#include "StaticMesh.h"
+#include "StaticMeshComponent.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace GraphicsUtils;
+using namespace Graphics;
+
+Core::MultiThreadApp::MultiThreadApp()
+{
+	rtvClearColor = { 0.53F, 0.81F, 0.92F, 1.0F };
+	m_aspectRatio = 1280.f / 720.f;
+}
+
+Core::MultiThreadApp::MultiThreadApp(const int width, const int height)
+{
+	rtvClearColor = { 0.53F, 0.81F, 0.92F, 1.0F };
+	m_aspectRatio = width / (float)height;
+}
+
+Core::MultiThreadApp::~MultiThreadApp()
+{
+}
+
+int Core::MultiThreadApp::Run()
+{
+	MSG msg = { };
+	m_timer.Reset();
+
+	std::thread renderThread([&] {
+		std::unique_lock<std::mutex> lock(g_mtx);
+		cv.wait(lock, [&] { return !isRunning || frameReady; });
+
+		if (!isRunning)
+			return;
+
+		frameReady = false;
+		lock.unlock();
+		Render(renderPSO);
+	});
+
+	while (isRunning) {
+		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {
+			if (msg.message == WM_QUIT)
+			{
+				isRunning = false;
+				break;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			m_timer.Tick();
+			deltaTime = (float)m_timer.GetDeltaTime();
+
+			PostActorChanges();
+			Update(deltaTime);
+			BuildProxy();
+
+			{
+				std::lock_guard<std::mutex> lock(g_mtx);
+				frameReady = true;
+			}
+
+			cv.notify_one();
+		}
+	}
+	isRunning = false;
+	{
+		std::lock_guard<std::mutex> lock(g_mtx);
+		frameReady = true;
+	}
+	cv.notify_all();
+	renderThread.join();
+
+	return (int)msg.wParam;
+}
+
+bool Core::MultiThreadApp::InitDirectX()
+{
+	UINT dxgiFactoryFlags = 0;
+
+	// Enable the debug layer
+#if defined(DEBUG) || defined(_DEBUG)
+	{
+		ComPtr<ID3D12Debug> debugController;
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+		{
+			debugController->EnableDebugLayer();
+			// Enable additional debug layers.
+			dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
+		}
+	}
+#endif
+
+	// Create Factory and Device 
+	ComPtr<ID3D12Device5> device;
+	ThrowIfFailed(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&m_dxgiFactory)));
+
+	HRESULT hardwareResult = D3D12CreateDevice(
+		nullptr,
+		D3D_FEATURE_LEVEL_11_0,
+		IID_PPV_ARGS(&device));
+
+	if (FAILED(hardwareResult))
+	{
+		ComPtr<IDXGIAdapter> pWarpAdapter;
+		m_dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&pWarpAdapter));
+		D3D12CreateDevice(
+			pWarpAdapter.Get(),
+			D3D_FEATURE_LEVEL_11_0,
+			IID_PPV_ARGS(&device));;
+	}
+
+	ThrowIfFailed(device.As(&m_device));
+
+	utility = std::make_shared<GraphicsUtils::Utility>(m_device.Get(), m_commandList.Get());
+	Graphics::InitializeCommonState(m_device);
+	Renderer::Initialize(m_device);
+
+	CreateCommandObjects();
+	
+	// Descriptor Handle offset 구하기
+	m_cbvSrvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+	m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	m_dsvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_DSV);
+	
+	// DescriptorHeap 생성
+	utility->CreateDescriptorHeap(m_swapChainBufferCount, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, m_swapChainRTVHeap);
+	utility->CreateDescriptorHeap(m_dsBufferCount, D3D12_DESCRIPTOR_HEAP_TYPE_DSV, m_DSVHeap);
+
+	CreateSwapChain();
+	CreateDepthBuffer();
+
+	// Create SwapChain RTVs
+	CD3DX12_CPU_DESCRIPTOR_HANDLE handle(m_swapChainRTVHeap->GetCPUDescriptorHandleForHeapStart());
+	for (int i = 0; i < m_swapChainBufferCount; i++)
+	{
+		m_swapChain->GetBuffer(i, IID_PPV_ARGS(m_swapChainResources[i].ReleaseAndGetAddressOf()));
+		m_device->CreateRenderTargetView(m_swapChainResources[i].Get(), nullptr, handle);
+
+		handle.Offset(1, m_rtvDescriptorSize);
+	}
+
+	m_viewport = CD3DX12_VIEWPORT(0.F, 0.F, (FLOAT)m_width, (FLOAT)m_height);
+	m_scissorRect = CD3DX12_RECT(0, 0, (LONG)m_width, (LONG)m_height);
+
+	CreateTextures();
+	{
+		m_commandAllocator->Reset();
+		m_commandList->Reset(m_commandAllocator.Get(), nullptr);
+
+		BuildGeometry();
+		BuildFrameResources();
+
+		m_commandList->Close();
+		ID3D12CommandList* commands[] = { m_commandList.Get() };
+		m_commandQueue->ExecuteCommandLists(ARRAYSIZE(commands), commands);
+
+		FlushCommands();
+	}
+	return true;
+}
+
+bool Core::MultiThreadApp::InitGUI()
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	// io.Fonts->TexID = (ImTextureID)m_guiFont->GetSpriteSheet().ptr;
+
+	ImGui::StyleColorsLight();
+	const char* fontPath = "Fonts/Hack-Regular.ttf";
+	float fontSize = 15.0f;
+	// 폰트 로드 
+	io.Fonts->AddFontFromFileTTF(fontPath, fontSize);
+
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+	heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	heapDesc.NumDescriptors = 1;
+	heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	m_device->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&m_guiFontHeap));
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplWin32_Init(m_mainWnd);
+
+	ImGui_ImplDX12_Init(m_device.Get(), m_swapChainBufferCount, Renderer::backBufferFormat,
+		m_guiFontHeap.Get(),
+		m_guiFontHeap->GetCPUDescriptorHandleForHeapStart(),
+		m_guiFontHeap->GetGPUDescriptorHandleForHeapStart());
+
+	return true;
+}
+
+void Core::MultiThreadApp::OnResize()
+{
+	if (m_swapChain == nullptr) return;
+
+	m_aspectRatio = m_width / (float)m_height;
+
+	// swapchain 버퍼 리셋
+	for (int i = 0; i < m_swapChainBufferCount; i++)
+	{
+		m_swapChainResources[i].Reset();
+	}
+
+	// swapchain 버퍼 크기 조정
+	m_swapChain->ResizeBuffers(m_swapChainBufferCount,
+		m_width,
+		m_height,
+		DXGI_FORMAT_UNKNOWN,
+		DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH);
+
+	// 버퍼에 대한 RTV 재생성
+	CD3DX12_CPU_DESCRIPTOR_HANDLE handle(m_swapChainRTVHeap->GetCPUDescriptorHandleForHeapStart());
+	for (int i = 0; i < m_swapChainBufferCount; i++)
+	{
+		m_swapChain->GetBuffer(i, IID_PPV_ARGS(m_swapChainResources[i].ReleaseAndGetAddressOf()));
+		m_device->CreateRenderTargetView(m_swapChainResources[i].Get(), nullptr, handle);
+
+		handle.Offset(1, m_rtvDescriptorSize);
+	}
+
+	// DepthBuffer 재생성
+	CreateDepthBuffer();
+
+	m_currentBackBufferIndex = m_swapChain->GetCurrentBackBufferIndex();
+
+	m_viewport = CD3DX12_VIEWPORT(0.F, 0.F, (FLOAT)m_width, (FLOAT)m_height);
+	m_scissorRect = CD3DX12_RECT(0, 0, (LONG)m_width, (LONG)m_height);
+
+}
+
+
+
+void Core::MultiThreadApp::CreateCommandObjects()
+{
+	ThrowIfFailed(
+		m_device->CreateCommandAllocator(
+			D3D12_COMMAND_LIST_TYPE_DIRECT,
+			IID_PPV_ARGS(&m_commandAllocator)
+		));
+
+	ThrowIfFailed(
+		m_device->CreateCommandList(
+			0,
+			D3D12_COMMAND_LIST_TYPE_DIRECT,
+			m_commandAllocator.Get(),
+			nullptr,
+			IID_PPV_ARGS(&m_commandList)
+		));
+
+	D3D12_COMMAND_QUEUE_DESC queueDesc;
+	ZeroMemory(&queueDesc, sizeof(queueDesc));
+	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	ThrowIfFailed(
+		m_device->CreateCommandQueue(
+			&queueDesc,
+			IID_PPV_ARGS(&m_commandQueue))
+	);
+
+	m_commandList->Close();
+
+}
+
+void Core::MultiThreadApp::CreateSwapChain()
+{
+	ComPtr<IDXGISwapChain1> swapChain;
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.BufferCount = m_swapChainBufferCount;
+	swapChainDesc.Width = m_width;
+	swapChainDesc.Height = m_height;
+	swapChainDesc.Format = Renderer::backBufferFormat;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDesc.SampleDesc.Count = 1;
+
+	ThrowIfFailed(m_dxgiFactory->CreateSwapChainForHwnd(
+		m_commandQueue.Get(),
+		m_mainWnd,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&swapChain
+	));
+
+	ThrowIfFailed(swapChain.As(&m_swapChain));
+
+	m_currentBackBufferIndex = m_swapChain->GetCurrentBackBufferIndex();
+}
+
+void Core::MultiThreadApp::CreateDepthBuffer()
+{
+	D3D12_RESOURCE_DESC rDesc = {};
+	rDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	rDesc.Format = Renderer::dsBufferFormat;
+	rDesc.MipLevels = 0;
+	rDesc.DepthOrArraySize = 1;
+	rDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+	rDesc.Width = m_width;
+	rDesc.Height = m_height;
+	rDesc.SampleDesc = { 1,0 };
+
+	D3D12_CLEAR_VALUE cValue = {};
+	cValue.DepthStencil.Depth = 1.f;
+	cValue.DepthStencil.Stencil = 0;
+	cValue.Format = Renderer::dsBufferFormat;
+
+	m_device->CreateCommittedResource(
+		&CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+		D3D12_HEAP_FLAG_NONE,
+		&rDesc,
+		D3D12_RESOURCE_STATE_COMMON,
+		&cValue,
+		IID_PPV_ARGS(m_depthStencilBuffer.ReleaseAndGetAddressOf())
+	);
+
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	dsvDesc.Texture2D.MipSlice = 0;
+	dsvDesc.Format = Renderer::dsBufferFormat;
+	dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
+
+	D3D12_CPU_DESCRIPTOR_HANDLE handle = m_DSVHeap->GetCPUDescriptorHandleForHeapStart();
+	m_device->CreateDepthStencilView(m_depthStencilBuffer.Get(), &dsvDesc, handle);
+
+}
+
+void Core::MultiThreadApp::UpdateGUI(float deltaTime)
+{
+	std::string str = "FPS : ";
+	str += std::to_string(int(1 / deltaTime));
+	ImGui::Text(str.c_str());
+}
+
+void Core::MultiThreadApp::BuildGeometry()
+{
+	
+	std::shared_ptr<Actor> sphereActor = std::make_shared<Actor>("sphere");
+
+	std::shared_ptr<StaticMesh> sphere1 = std::make_shared<StaticMesh>();
+	sphere1->Initialize(m_device.Get(), m_commandList.Get(), GeometryGenerator::MakeSphere(100, 1));
+	sphere1->SetAlbedoTexture("8k_earth_albedo");
+	sphere1->SetLocation(0, 0, 3);
+
+	std::shared_ptr<StaticMeshComponent> cmp = std::make_shared<StaticMeshComponent>(sphereActor.get());
+	cmp->SetMesh(sphere1);
+
+	sphereActor->SetRootComponent(cmp);
+
+	m_actors.push_back(sphereActor);
+	
+	
+}
+
+void Core::MultiThreadApp::BuildFrameResources()
+{
+	for (int i = 0; i < m_frameResourceCount; i++)
+	{
+		m_frameResources[i].Initialize();
+	}
+}
+
+void Core::MultiThreadApp::PostActorChanges()
+{
+}
+
+void Core::MultiThreadApp::Update(float deltaTime)
+{
+	// 0번 프레임일 경우
+}
+
+void Core::MultiThreadApp::BuildProxy()
+{
+}
+
+void Core::MultiThreadApp::Render(const std::string& psoName)
+{
+	using namespace Renderer;
+
+	GraphicsPSO pso;
+	if (m_PSOs.find(psoName) != m_PSOs.end())
+	{
+		pso = m_PSOs[psoName];
+	}
+	else
+	{
+		pso = m_PSOs["defaultPSO"];
+	}
+
+
+	m_commandAllocator->Reset();
+	m_commandList->Reset(m_commandAllocator.Get(), pso.GetPSO());
+
+	m_commandList->RSSetScissorRects(1, &m_scissorRect);
+	m_commandList->RSSetViewports(1, &m_viewport);
+
+	m_commandList->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	m_commandList->SetPipelineState(pso.GetPSO());
+	m_commandList->SetGraphicsRootSignature(pso.GetRootSignature()->GetSignature());
+
+
+	m_commandList->ResourceBarrier(
+		1,
+		&CD3DX12_RESOURCE_BARRIER::Transition(
+			GetCurrentSwapChainResource(),
+			D3D12_RESOURCE_STATE_PRESENT,
+			D3D12_RESOURCE_STATE_RENDER_TARGET
+		));
+
+	m_commandList->ClearRenderTargetView(GetCurrentRtvCpuHandle(), rtvClearColor.data(), 0, nullptr);
+	m_commandList->ClearDepthStencilView(GetDSVCpuHandle(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.f, 0, 0, nullptr);
+	m_commandList->OMSetRenderTargets(1, &GetCurrentRtvCpuHandle(), TRUE, &GetDSVCpuHandle());
+
+
+	ID3D12DescriptorHeap* heaps[] = {
+		m_textureLoader->GetHeap()
+	};
+
+	m_commandList->SetDescriptorHeaps(1, heaps);
+
+
+	if (psoName == "phongPSO")
+	{
+		/*m_commandList->SetGraphicsRootConstantBufferView(2, m_phongGCB->GetGPUVirtualAddress());
+		for (auto& mesh : phongMeshes)
+		{
+			mesh->Render(m_commandList.Get(), m_textureLoader.get());
+		}*/
+	}
+
+	m_commandList->ResourceBarrier(
+		1,
+		&CD3DX12_RESOURCE_BARRIER::Transition(
+			GetCurrentSwapChainResource(),
+			D3D12_RESOURCE_STATE_RENDER_TARGET,
+			D3D12_RESOURCE_STATE_PRESENT
+		));
+
+	m_commandList->Close();
+
+	ID3D12CommandList* commands[] = { m_commandList.Get() };
+	m_commandQueue->ExecuteCommandLists(ARRAYSIZE(commands), commands);
+
+}
+
+void Core::MultiThreadApp::CreateTextures()
+{
+	texturePath = "Build/";
+	fallbackPath = "Build/Fallback/";
+
+	DDSPath = "Textures/DDS/";
+	fallbackDDSPath = "Textures/Falback/";
+
+	m_textureLoader = std::make_shared<TextureLoader>(texturePath);
+	m_fallbackLoader = std::make_shared<TextureLoader>(fallbackPath);
+
+	m_textureLoader->LoadIdx(m_device);
+	m_fallbackLoader->LoadIdx(m_device);
+	m_textureLoader->LoadTextures(m_device, m_commandQueue);
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE Core::MultiThreadApp::GetCurrentRtvCpuHandle() const
+{
+	return CD3DX12_CPU_DESCRIPTOR_HANDLE(m_swapChainRTVHeap->GetCPUDescriptorHandleForHeapStart(), m_currentBackBufferIndex, m_rtvDescriptorSize);
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE Core::MultiThreadApp::GetDSVCpuHandle() const
+{
+	return CD3DX12_CPU_DESCRIPTOR_HANDLE(m_DSVHeap->GetCPUDescriptorHandleForHeapStart());
+}
+
+ID3D12Resource* Core::MultiThreadApp::GetCurrentSwapChainResource() const
+{
+	return m_swapChainResources[m_currentBackBufferIndex].Get();
+}
+
+void Core::MultiThreadApp::FlushCommands()
+{
+	m_currentFence++;
+
+	ThrowIfFailed(m_commandQueue->Signal(m_fence.Get(), m_currentFence));
+
+	if (m_fence->GetCompletedValue() < m_currentFence)
+	{
+		HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
+		m_fence->SetEventOnCompletion(m_currentFence, eventHandle);
+
+		WaitForSingleObject(eventHandle, INFINITE);
+		CloseHandle(eventHandle);
+	}
+}
