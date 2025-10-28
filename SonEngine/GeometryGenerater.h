438615dbc3581b076e54bb9ba5da535eddd5c8e0
SonEngine <214961477+SonEngine@users.noreply.github.com>
2025-10-28 21:08:19 +0900

    Update GeometryGenerater.h

--- SonEngine/GeometryGenerater.h
+++ SonEngine/GeometryGenerater.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "Mesh.h"
 #include "Vertex.h"
@@ -7,6 +7,8 @@ struct GeometryGenerator
 {
 	static Mesh<SimpleVertex, uint16_t> MakeSimpleRect(float x, float y);
 	static Mesh<SimpleVertex, uint16_t> MakeSimpleCube(float x, float y, float z);
+
+	static Mesh<Vertex, uint16_t> MakeCube(float x, float y, float z);
 
 	static Mesh<Vertex, uint16_t> MakePlane(float x, float z, int c);
 
