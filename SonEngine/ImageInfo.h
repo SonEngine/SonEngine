4021481c124e378b3ec7a4053a273f9aaa8cf947
SonEngine <214961477+SonEngine@users.noreply.github.com>
2025-11-15 11:44:07 +0900

    Create ImageInfo.h

---
+++ SonEngine/ImageInfo.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <string>
+
+struct ImageInfo {
+	std::string name;
+	long long bufferPixelCount;
+	long long numRows;
+	long long rowSize;
+	long long rowPitch;
+	long long width;
+	long long height;
+};
